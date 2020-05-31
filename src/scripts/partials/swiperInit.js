879da29203b5f6e4077fd426145ebcf1f56c1420
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2020-05-31 05:33:01 +0200

    working on contact us, clients, latest news sections in index.html page

--- src/scripts/partials/swiperInit.js
+++ src/scripts/partials/swiperInit.js
@@ -2,10 +2,14 @@
 
 var Swiper = require('swiper');
 module.exports = function () {
-	var $swiper_containers = $('.swiper-container');
-	if ($swiper_containers && $swiper_containers.length) {
-		var effect_array = ["slide", "fade", "cube", "flip", "coverflow"];
-		$swiper_containers.each(function(index, ele){
+	function css_variable(var_name) {
+		return getComputedStyle(document.documentElement).getPropertyValue(var_name);
+	};
+	var $single_slide_swiper = $('#home-banner-slider, #home-whatWeDo-slider');
+	var $clients_swiper = $("#home-clients-slider");
+	var effect_array = ["slide", "fade", "cube", "flip", "coverflow"];
+	if ($single_slide_swiper.length) {
+		$single_slide_swiper.each(function (index, ele) {
 			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
 			new Swiper(`#${$(ele).attr("id")}`, {
 				init: true,
@@ -14,7 +18,9 @@ module.exports = function () {
 				loop: true,
 				parallax: true,
 				grabCursor: true,
-				mousewheel: true,
+				autoplay: {
+					delay: 6000
+				},
 				keyboard: {
 					enabled: true,
 					onlyInViewport: true
@@ -27,8 +33,41 @@ module.exports = function () {
 					shadow: false
 				},
 				navigation: {
-					nextEl: '.swiper-button-next',
-					prevEl: '.swiper-button-prev'
+					nextEl: $(ele).closest("section").find(".swiper-button-next"),
+					prevEl: $(ele).closest("section").find(".swiper-button-prev")
+				}
+			});
+		});
+	}
+
+	if ($clients_swiper.length) {
+		$clients_swiper.each(function (index, ele) {
+			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+			new Swiper(`#${$(ele).attr("id")}`, {
+				init: true,
+				direction: 'horizontal',
+				speed: 1000,
+				loop: true,
+				parallax: true,
+				slidesPerView: 4,
+				spaceBetween: 30,
+				autoplay: {
+					delay: 6000
+				},
+				keyboard: {
+					enabled: true,
+					onlyInViewport: true
+				},
+				effect: effect,
+				fadeEffect: {
+					crossFade: true
+				},
+				cubeEffect: {
+					shadow: false
+				},
+				navigation: {
+					nextEl: $(ele).closest("section").find(".swiper-button-next"),
+					prevEl: $(ele).closest("section").find(".swiper-button-prev")
 				}
 			});
 		});
