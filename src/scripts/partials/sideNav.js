637197f39afe7bd69033adceb50939473545a161
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2020-06-01 04:58:31 +0200

    edited header clases

--- src/scripts/partials/sideNav.js
+++ src/scripts/partials/sideNav.js
@@ -13,45 +13,45 @@ module.exports = (options = {entrance_direction: 'left',exit_direction: 'left',s
 			this._bind_events();
 		},
 		_cache_dom: function () {
-			this.$body       = $('body');
-			this.$menu       = $('.overlay');
-			this.$bg_overlay = $('.side-open');
-			this.$open_icon  = $('.menu-open');
-			this.$close_icon = this.$menu.find('.menu-close');
+			this.$body          = $('body');
+			this.$menu          = $('.menu');
+			this.$menu_underlay = $('.menu__underlay');
+			this.$open_icon     = $('.menu__open-button');
+			this.$close_icon    = this.$menu.find('.menu__close-button');
 		},
 		_bind_events: function () {
 			var _this = this; // refer to side_nav
 			this.$body.on('keyup', function (e) {if (isEqual(e.key, 'Escape') && isEqual(e.which, 27)) {_this._close_menu();}});
 			this.$open_icon.on('click',  this._open_menu.bind(this));
 			this.$close_icon.on('click', this._close_menu.bind(this));
-			this.$bg_overlay.on('click', this._close_menu.bind(this));
+			this.$menu_underlay.on('click', this._close_menu.bind(this));
 		},
 		_open_menu: function (e) {
 
 			// checking if side nav is closed
-			if (!this.$menu.hasClass('open') && !this.$bg_overlay.hasClass('d-block')) {
+			if (!this.$menu.hasClass('is_open') && !this.$menu_underlay.hasClass('d-block')) {
 
-				// fade in side nav background body overlay
-				this.$bg_overlay.addClass('d-block').animateCss(`fadeIn ${this._animation.speed}`);
+				// fade in side nav background body underlay
+				this.$menu_underlay.addClass('d-block').animateCss(`fadeIn ${this._animation.speed}`);
 
 				// open side nav
-				this.$menu.addClass('open').animateCss(this._animation.entrance);
+				this.$menu.addClass('is_open').animateCss(this._animation.entrance);
 			}
 		},
 		_close_menu: function (e) {
 			var _this = this; // refer to side_nav
 
 			// checking if side nav is open
-			if ((this.$menu.hasClass('open') && this.$bg_overlay.hasClass('d-block'))) {
+			if ((this.$menu.hasClass('is_open') && this.$menu_underlay.hasClass('d-block'))) {
 
-				// fade out side nav background body overlay
-				this.$bg_overlay.animateCss(`fadeOut ${this._animation.speed}`, function () {
-					_this.$bg_overlay.removeClass('d-block');
+				// fade out side nav background body underlay
+				this.$menu_underlay.animateCss(`fadeOut ${this._animation.speed}`, function () {
+					_this.$menu_underlay.removeClass('d-block');
 				});
 
 				// exit side nav
 				this.$menu.animateCss(this._animation.exit, function () {
-					_this.$menu.removeClass(`${_this._animation.exit} open`);
+					_this.$menu.removeClass(`${_this._animation.exit} is_open`);
 				});
 			}
 		}
