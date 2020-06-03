67a796a4d079e841314637b9f25527d23e319d58
Mohamed Ashraf Othman <mohamedashrafothman@gmail.com>
2020-06-03 03:40:24 +0200

    finishing about-our-history page with responsivenes

--- src/scripts/partials/sideNav.js
+++ src/scripts/partials/sideNav.js
@@ -14,44 +14,44 @@ module.exports = (options = {entrance_direction: 'left',exit_direction: 'left',s
 		},
 		_cache_dom: function () {
 			this.$body            = $('body');
-			this.$navbar          = $('.navbar__side-menu');
-			this.$navbar_underlay = $('.navbar__underlay');
-			this.$open_icon       = $('.navbar__open-button');
-			this.$close_icon      = this.$navbar.find('.navbar__close-button');
+			this.$app_navbar          = $('.app-navbar__side-menu');
+			this.$app_navbar_underlay = $('.app-navbar__underlay');
+			this.$open_icon       = $('.app-navbar__open-button');
+			this.$close_icon      = this.$app_navbar.find('.app-navbar__close-button');
 		},
 		_bind_events: function () {
 			var _this = this;  // refer to side_nav
-			this.$body.on('keyup', function (e) {if (isEqual(e.key, 'Escape') && isEqual(e.which, 27)) {_this._close_navbar();}});
-			this.$open_icon.on('click',  this._open_navbar.bind(this));
-			this.$close_icon.on('click', this._close_navbar.bind(this));
-			this.$navbar_underlay.on('click', this._close_navbar.bind(this));
+			this.$body.on('keyup', function (e) {if (isEqual(e.key, 'Escape') && isEqual(e.which, 27)) {_this._close_app_navbar();}});
+			this.$open_icon.on('click',  this._open_app_navbar.bind(this));
+			this.$close_icon.on('click', this._close_app_navbar.bind(this));
+			this.$app_navbar_underlay.on('click', this._close_app_navbar.bind(this));
 		},
-		_open_navbar: function (e) {
+		_open_app_navbar: function (e) {
 
 			// checking if side nav is closed
-			if (!this.$navbar.hasClass('is_open') && !this.$navbar_underlay.hasClass('d-block')) {
+			if (!this.$app_navbar.hasClass('is_open') && !this.$app_navbar_underlay.hasClass('d-block')) {
 
 				// fade in side nav background body underlay
-				this.$navbar_underlay.addClass('d-block').animateCss(`fadeIn ${this._animation.speed}`);
+				this.$app_navbar_underlay.addClass('d-block').animateCss(`fadeIn ${this._animation.speed}`);
 
 				// open side nav
-				this.$navbar.addClass('is_open').animateCss(this._animation.entrance);
+				this.$app_navbar.addClass('is_open').animateCss(this._animation.entrance);
 			}
 		},
-		_close_navbar: function (e) {
+		_close_app_navbar: function (e) {
 			var _this = this;  // refer to side_nav
 
 			// checking if side nav is open
-			if ((this.$navbar.hasClass('is_open') && this.$navbar_underlay.hasClass('d-block'))) {
+			if ((this.$app_navbar.hasClass('is_open') && this.$app_navbar_underlay.hasClass('d-block'))) {
 
 				// fade out side nav background body underlay
-				this.$navbar_underlay.animateCss(`fadeOut ${this._animation.speed}`, function () {
-					_this.$navbar_underlay.removeClass('d-block');
+				this.$app_navbar_underlay.animateCss(`fadeOut ${this._animation.speed}`, function () {
+					_this.$app_navbar_underlay.removeClass('d-block');
 				});
 
 				// exit side nav
-				this.$navbar.animateCss(this._animation.exit, function () {
-					_this.$navbar.removeClass(`${_this._animation.exit} is_open`);
+				this.$app_navbar.animateCss(this._animation.exit, function () {
+					_this.$app_navbar.removeClass(`${_this._animation.exit} is_open`);
 				});
 			}
 		}
--- src/scripts/partials/swiperInit.js
+++ src/scripts/partials/swiperInit.js
@@ -2,48 +2,40 @@
 
 var Swiper = require('swiper');
 module.exports = function () {
-	function css_variable(var_name) {
-		return getComputedStyle(document.documentElement).getPropertyValue(var_name);
-	};
-	var $single_slide_swiper = $('#home-banner-slider, #home-whatWeDo-slider');
+	var $single_slide_swiper = $('div[id^="home-banner-slider"], div[id^="home-whatWeDo-slider"], div[id^="timeline-slider"]:not([id*="-prev"]):not([id*="-next"])');
 	var $clients_swiper = $("#home-clients-slider");
 	var effect_array = ["slide", "fade", "cube", "flip", "coverflow"];
+
+	// FIXME: fix multiple slider in the same page with bootstrap tabs issue.
+
 	if ($single_slide_swiper.length) {
 		$single_slide_swiper.each(function (index, ele) {
 			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
-			new Swiper(`#${$(ele).attr("id")}`, {
+			var swiper_config = {
 				init: true,
 				direction: 'horizontal',
 				speed: 1000,
 				loop: true,
 				parallax: true,
 				grabCursor: true,
-				// autoplay: {
-				// 	delay: 6000
-				// },
-				keyboard: {
-					enabled: true,
-					onlyInViewport: true
-				},
 				effect: effect,
-				fadeEffect: {
-					crossFade: true
-				},
-				cubeEffect: {
-					shadow: false
-				},
+				keyboard: { enabled: true, onlyInViewport: true },
+				fadeEffect: { crossFade: true },
+				cubeEffect: { shadow: false },
 				navigation: {
-					nextEl: $(ele).closest("section").find(".swiper-button-next"),
-					prevEl: $(ele).closest("section").find(".swiper-button-prev")
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
 				}
-			});
+			};
+
+			var swiper = new Swiper(`#${$(ele).attr("id")}`, swiper_config);
 		});
 	}
 
 	if ($clients_swiper.length) {
 		$clients_swiper.each(function (index, ele) {
 			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
-			new Swiper(`#${$(ele).attr("id")}`, {
+			var swiper_config = {
 				init: true,
 				direction: 'horizontal',
 				speed: 1000,
@@ -51,25 +43,17 @@ module.exports = function () {
 				parallax: true,
 				slidesPerView: 4,
 				spaceBetween: 30,
-				// autoplay: {
-				// 	delay: 6000
-				// },
-				keyboard: {
-					enabled: true,
-					onlyInViewport: true
-				},
 				effect: effect,
-				fadeEffect: {
-					crossFade: true
-				},
-				cubeEffect: {
-					shadow: false
-				},
+				keyboard: { enabled: true, onlyInViewport: true },
+				fadeEffect: { crossFade: true },
+				cubeEffect: { shadow: false },
 				navigation: {
-					nextEl: $(ele).closest("section").find(".swiper-button-next"),
-					prevEl: $(ele).closest("section").find(".swiper-button-prev")
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
 				}
-			});
+			};
+
+			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
 		});
 	}
 };
