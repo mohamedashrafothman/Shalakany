f3769075853d44f2ae466aac327d4d3a4e81b1f9
mohamed ashraf othman <mashraf@road9media.com>
2020-11-17 16:43:24 +0200

    added new gallery pages and edited home page

--- src/scripts/partials/swiperInit.js
+++ src/scripts/partials/swiperInit.js
@@ -1,7 +1,10 @@
-'use strict';
+"use strict";
 
 module.exports = function () {
-	var $single_slide_swiper = $('div[id^="home-banner-slider"], div[id^="home-whatWeDo-slider"]');
+	var $hero_slider = $("#home-banner-slider");
+	var $hero_slider_images = $("#home-banner-slider-images");
+
+	var $what_we_do_slider = $('div[id^="home-whatWeDo-slider"]');
 	var $timeline_swiper = $('div[id^="timeline-slider"]:not([class*="-prev"]):not([class*="-next"]):not([class*="-pagination"])');
 	var $clients_swiper = $("#clients-slider");
 	var $gallery_swiper = $("#gallery-slider");
@@ -9,16 +12,57 @@ module.exports = function () {
 	var $firms_culture_swiper = $("#firms-culture-slider");
 	var effect_array = ["slide", "fade", "cube", "flip", "coverflow"];
 
-	if ($single_slide_swiper.length) {
-		$single_slide_swiper.each(function (index, ele) {
-			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+	if ($hero_slider.length) {
+		var swiper_config = {
+			direction: "horizontal",
+			speed: 1000,
+			autoplay: { delay: 5000, disableOnInteraction: true },
+			parallax: true,
+			slidesPerView: 1,
+			autoHeight: true,
+			effect: "fade",
+			fadeEffect: {
+				crossFade: true,
+			},
+			keyboard: {
+				enabled: true,
+				onlyInViewport: true,
+			},
+			navigation: {
+				prevEl: `div.swiper-button-prev[for^="${$hero_slider.attr("id")}"]`,
+				nextEl: `div.swiper-button-next[for^="${$hero_slider.attr("id")}"]`,
+			},
+		};
+
+		var swiper_images_config = {
+			speed: 1000,
+			slidesPerView: 1,
+			freeMode: false,
+			watchSlidesVisibility: true,
+			watchSlidesProgress: true,
+			effect: "fade",
+			fadeEffect: {
+				crossFade: true,
+			},
+		};
+
+		const slider = new Swiper(`#${$hero_slider.attr("id")}`, swiper_config);
+		const slider_images = new Swiper(`#${$hero_slider_images.attr("id")}`, swiper_images_config);
+
+		slider.on("slideChange", () => {
+			slider_images.slideTo(slider.activeIndex);
+		});
+		slider_images.on("slideChange", () => {
+			slider.slideTo(slider_images.activeIndex);
+		});
+	}
+	if ($what_we_do_slider.length) {
+		$what_we_do_slider.each(function (index, ele) {
+			var effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
 			var swiper_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
-				// autoplay: {
-				// 	delay: 5000,
-				// },
 				parallax: true,
 				grabCursor: true,
 				effect: effect,
@@ -26,18 +70,18 @@ module.exports = function () {
 				observeParents: true,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
-				}
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
+				},
 			};
 
 			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
@@ -46,10 +90,12 @@ module.exports = function () {
 
 	if ($timeline_swiper.length) {
 		$timeline_swiper.each(function (index, ele) {
-			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
-			var bullets_array = $(`div.swiper-pagination[for^="${$(ele).attr('id')}"]`).attr("data-pagination-bullets").split(",");
+			var effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
+			var bullets_array = $(`div.swiper-pagination[for^="${$(ele).attr("id")}"]`)
+				.attr("data-pagination-bullets")
+				.split(",");
 			var swiper_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
 				parallax: true,
@@ -59,20 +105,20 @@ module.exports = function () {
 				observeParents: true,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
 				},
 				pagination: {
-					el: `div.swiper-pagination[for^="${$(ele).attr('id')}"] .timeline__pagination-list`,
+					el: `div.swiper-pagination[for^="${$(ele).attr("id")}"] .timeline__pagination-list`,
 					clickable: true,
 					renderBullet: function (index, className) {
 						return `
@@ -80,9 +126,8 @@ module.exports = function () {
 								<span class="timeline__pagination-link">${bullets_array[index]}</span>
 							</li>
 						`;
-
-					}
-				}
+					},
+				},
 			};
 			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
 		});
@@ -90,9 +135,9 @@ module.exports = function () {
 
 	if ($clients_swiper.length) {
 		$clients_swiper.each(function (index, ele) {
-			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+			var effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
 			var swiper_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
 				// autoplay: {
@@ -103,47 +148,47 @@ module.exports = function () {
 				effect: effect,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
 				},
-				slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+				slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
 				// Responsive breakpoints
 				breakpoints: {
 					// when window width is >= 0px ------ xs
 					0: {
 						slidesPerView: 1,
-						slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+						slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
 					},
 					// when window width is >= 576px ------ small
 					576: {
 						slidesPerView: 2,
-						slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+						slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
 					},
 					// when window width is >= 768px  ------ medium
 					768: {
 						slidesPerView: 3,
-						slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+						slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
 					},
 					// when window width is >= 992px  ------ large
 					992: {
 						slidesPerView: 4,
-						slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+						slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
 					},
 					// when window width is >= 1200px  ------ extra large
 					1200: {
 						slidesPerView: 4,
-						slidesPerColumn: ($(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn"))) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
-					}
-				}
+						slidesPerColumn: $(ele).attr("data-swiper-slidesPerColumn") && Boolean($(ele).attr("data-swiper-slidesPerColumn")) ? Number($(ele).attr("data-swiper-slidesPerColumn")) : 1,
+					},
+				},
 			};
 
 			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
@@ -152,9 +197,9 @@ module.exports = function () {
 
 	if ($gallery_swiper.length) {
 		$gallery_swiper.each(function (index, ele) {
-			var slider_effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+			var slider_effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
 			var gallery_slider_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
 				// autoplay: {
@@ -166,33 +211,32 @@ module.exports = function () {
 				effect: slider_effect,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
 				},
 			};
 			var gallery_slider = new Swiper(`#${$(ele).attr("id")}`, gallery_slider_config);
 
-
-			var $thumb = $(ele).closest('section').find("#gallery-thumbs");
+			var $thumb = $(ele).closest("section").find("#gallery-thumbs");
 			var gallery_thumbs_config = {
 				loop: true,
 				spaceBetween: 10,
 				centeredSlides: true,
-				slidesPerView: 'auto',
+				slidesPerView: "auto",
 				// autoplay: {
 				// 	delay: 5000,
 				// },
 				slideToClickedSlide: true,
-				loopedSlides: 4
+				loopedSlides: 4,
 			};
 			var gallery_thumbs = new Swiper(`#${$thumb.attr("id")}`, gallery_thumbs_config);
 
@@ -203,9 +247,9 @@ module.exports = function () {
 
 	if ($opinions_swiper.length) {
 		$opinions_swiper.each(function (index, ele) {
-			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+			var effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
 			var swiper_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
 				// autoplay: {
@@ -216,20 +260,20 @@ module.exports = function () {
 				centeredSlides: true,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
 				},
 				spaceBetween: 30,
-				slidesPerView: 'auto',
+				slidesPerView: "auto",
 				// Responsive breakpoints
 				breakpoints: {
 					// when window width is >= 0px ------ xs
@@ -238,9 +282,9 @@ module.exports = function () {
 					},
 					// when window width is >= 768px  ------ medium
 					768: {
-						slidesPerView: 'auto',
-					}
-				}
+						slidesPerView: "auto",
+					},
+				},
 			};
 
 			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
@@ -249,26 +293,26 @@ module.exports = function () {
 
 	if ($firms_culture_swiper.length) {
 		$firms_culture_swiper.each(function (index, ele) {
-			var effect = ($(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect"))) ? $(ele).attr("data-swiper-effect") : "slide";
+			var effect = $(ele).attr("data-swiper-effect") && effect_array.includes($(ele).attr("data-swiper-effect")) ? $(ele).attr("data-swiper-effect") : "slide";
 			var swiper_config = {
-				direction: 'horizontal',
+				direction: "horizontal",
 				speed: 1000,
 				loop: true,
 				grabCursor: true,
 				effect: effect,
 				keyboard: {
 					enabled: true,
-					onlyInViewport: true
+					onlyInViewport: true,
 				},
 				fadeEffect: {
-					crossFade: true
+					crossFade: true,
 				},
 				cubeEffect: {
-					shadow: false
+					shadow: false,
 				},
 				navigation: {
-					prevEl: `div.swiper-button-prev[for^="${$(ele).attr('id')}"]`,
-					nextEl: `div.swiper-button-next[for^="${$(ele).attr('id')}"]`
+					prevEl: `div.swiper-button-prev[for^="${$(ele).attr("id")}"]`,
+					nextEl: `div.swiper-button-next[for^="${$(ele).attr("id")}"]`,
 				},
 				spaceBetween: 30,
 				slidesPerView: 3,
@@ -285,8 +329,8 @@ module.exports = function () {
 					// when window width is >= 992px  ------ large
 					992: {
 						slidesPerView: 3,
-					}
-				}
+					},
+				},
 			};
 
 			new Swiper(`#${$(ele).attr("id")}`, swiper_config);
